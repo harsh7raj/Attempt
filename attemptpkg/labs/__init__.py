29f24faf27162f0f9e05399c2333e04952145139
Harsh Raj <harshraj.ai23@bmsce.ac.in>
2025-11-24 05:43:58 +0530

    Added all_labs.py and lab functions

---
+++ attemptpkg/labs/__init__.py
@@ -0,0 +1,13 @@
+from .all_labs import (
+    show_lab1,
+    show_lab2,
+    show_lab3,
+    show_lab4,
+    show_lab5,
+    show_lab6,
+    show_lab7,
+    show_lab8,
+    show_lab9,
+    show_lab10
+)
+
---
+++ attemptpkg/labs/all_labs.py
@@ -0,0 +1,70 @@
+# This file contains all lab programs with show_labX() printers.
+
+def show_lab1():
+    code = """
+# LAB 1 CODE HERE
+"""
+    print(code)
+
+
+def show_lab2():
+    code = """
+# LAB 2 CODE HERE
+"""
+    print(code)
+
+
+def show_lab3():
+    code = """
+# LAB 3 CODE HERE
+"""
+    print(code)
+
+
+def show_lab4():
+    code = """
+# LAB 4 CODE HERE
+"""
+    print(code)
+
+
+def show_lab5():
+    code = """
+# LAB 5 CODE HERE
+"""
+    print(code)
+
+
+def show_lab6():
+    code = """
+# LAB 6 CODE HERE
+"""
+    print(code)
+
+
+def show_lab7():
+    code = """
+# LAB 7 CODE HERE
+"""
+    print(code)
+
+
+def show_lab8():
+    code = """
+# LAB 8 CODE HERE
+"""
+    print(code)
+
+
+def show_lab9():
+    code = """
+# LAB 9 CODE HERE
+"""
+    print(code)
+
+
+def show_lab10():
+    code = """
+# LAB 10 CODE HERE
+"""
+    print(code)
